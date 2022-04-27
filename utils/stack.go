846f6c78545e3a04859a0d5313e6d137f8230a60
I ekwaL V <ekwal@protonmail.com>
2022-04-27 22:18:02 +0300

    feat(utils): add stack data structure

---
+++ utils/stack.go
@@ -0,0 +1,53 @@
+package utils
+
+type Stack[T any] interface {
+	Push(T)
+	Pop() (T, bool)
+	Peek() (T, bool)
+	IsEmpty() bool
+}
+
+type stack[T any] struct {
+	arr []T
+	top int
+}
+
+func NewStack[T any]() *stack[T] {
+	return &stack[T]{
+		arr: make([]T, 0, 100),
+		top: -1,
+	}
+}
+
+func (s *stack[T]) Push(el T) {
+	if s.top >= len(s.arr)-1 {
+		s.arr = append(s.arr, el)
+		s.top++
+	} else {
+		s.top++
+		s.arr[s.top] = el
+	}
+}
+
+func (s *stack[T]) Pop() (T, bool) {
+	if s.IsEmpty() {
+		return *new(T), false
+	}
+
+	el := s.arr[s.top]
+	s.top--
+	return el, true
+}
+
+func (s *stack[T]) Peek() (T, bool) {
+	if s.IsEmpty() {
+		return *new(T), false
+	}
+
+	el := s.arr[s.top]
+	return el, true
+}
+
+func (s *stack[T]) IsEmpty() bool {
+	return s.top == -1
+}
---
+++ utils/stack_test.go
@@ -0,0 +1,88 @@
+package utils_test
+
+import (
+	"monkey/utils"
+	"testing"
+)
+
+func TestStack(t *testing.T) {
+	s := utils.NewStack[int]()
+
+	if !s.IsEmpty() {
+		t.Error("Stack should be empty but it's not.")
+	}
+
+	if _, ok := s.Pop(); ok {
+		t.Error("Expected error on Pop, but got none.")
+	}
+
+	if _, ok := s.Peek(); ok {
+		t.Error("Expected error on Peek, but got none.")
+	}
+
+	if !s.IsEmpty() {
+		t.Error("Stack should be empty but it's not.")
+	}
+
+	push := []int{1, 2, 3, 4, 5}
+	pop := []int{5, 4, 3, 2, 1}
+
+	for _, p := range push {
+		s.Push(p)
+	}
+
+	if s.IsEmpty() {
+		t.Error("Stack should not be empty but it is.")
+	}
+
+	got, ok := s.Peek()
+	if !ok {
+		t.Error("Unexpected error on Peek")
+	}
+
+	want := 5
+	if got != want {
+		t.Errorf("Got wrong element on Peek. Want %v got %v.", want, got)
+	}
+
+	for _, want := range pop {
+		got, ok := s.Pop()
+		if !ok {
+			t.Error("Unexpected error on Pop")
+		}
+		if got != want {
+			t.Errorf("Got wrong element on Pop. Want %v got %v.", want, got)
+		}
+	}
+
+	if !s.IsEmpty() {
+		t.Error("Stack should be empty but it's not.")
+	}
+
+	if _, ok = s.Peek(); ok {
+		t.Error("Expected error on Peek, but got none.")
+	}
+
+	i := 0
+	for i = 0; i < 10000; i++ {
+		s.Push(i)
+	}
+
+	for i := 10000 - 1; i >= 0; i-- {
+		got, ok := s.Pop()
+		if !ok {
+			t.Fatal("Unexpected error on Pop.")
+		}
+		if got != i {
+			t.Fatalf("Got wrong element on Pop. Want %v got %v.", i, got)
+		}
+	}
+
+	if !s.IsEmpty() {
+		t.Error("Stack should be empty but it's not.")
+	}
+
+	if _, ok = s.Peek(); ok {
+		t.Error("Expected error on Peek, but got none.")
+	}
+}
